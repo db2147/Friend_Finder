41aa159a0ba87d0c44b3cd5fcaa4dc4e375d56d5
David Bierma <31113045+db2147@users.noreply.github.com>
2017-12-16 19:18:34 -0600

    building survey page

---
+++ app/routing/api-routes.js
@@ -0,0 +1,21 @@
+
+var friendsData = require('../data/friends.js'); // created a new variable which requires our friends.js page
+
+module.exports = function (app) {
+
+    app.get('/api/friends', function (req,res) {
+        res.json(friendsData);  //  
+    });
+
+    app.post("/api/friends", function(req, res) {  // when we create a post to this route, I want you to run this function
+        var newFriend = req.body;   // body came from the body parser function
+        //newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+    
+        console.log(newFriend);
+    
+        characters.push(newFriend); // pushes new character into existing characters array
+    
+        res.json(newFriend);  // send new character as a json
+    
+    });
+}
---
+++ app/routing/html-routes.js
@@ -0,0 +1,13 @@
+var path = require('path');
+
+module.exports = function (app) {
+
+    app.get('/survey', function(req, res) {  // using our get request to designate path
+        res.sendFile(path.join(__dirname + '/../public/survey.html'));
+    });
+
+    app.use('/', function(req, res) {                                   //saying whenever you get this ("/") for the root, we want to execute this function
+        res.sendFile(path.join(__dirname + '/../public/home.html'));
+    });
+
+}
