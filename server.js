96b89efe1bc92d5d8b756f1417a6d9bc7dc80786
David Bierma <31113045+db2147@users.noreply.github.com>
2017-12-16 18:02:33 -0600

    setting up routes

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-var express = require('express')
-var app = express()
+var bodyParser = require('body-parser');
+var express = require('express');
+var path = require('path');  // this is needed so we can write the routes
+
+var app = express();
+var PORT = process.env.PORT || 3000;  // this port will work on our localhost
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT); 
+    });
