df0e4f9e0d71a6c3bc51e2538c59a08dbe166b0c
David Bierma <31113045+db2147@users.noreply.github.com>
2017-12-19 17:25:50 -0600

    revise server.js

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ var app = express();
 var PORT = process.env.PORT || 3000;  // this port will work on our localhost
  
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ 
+    extended: false 
+}))
 
 // parse application/json
 app.use(bodyParser.json())
