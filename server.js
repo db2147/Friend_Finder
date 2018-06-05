8efa73f5ddacc84006b4b89726664dc833451c93
KrisB <david.bierma@gmail.com>
2018-06-05 13:34:45 -0500

    update comments

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');  // path dependency is needed so we can write the routes
 
 var app = express();
-var PORT = process.env.PORT || 3000;  // this port will work on our localhost
+var PORT = process.env.PORT || 3000;  // creating our port which is used below and using the logical or operator to specify port number
  
 // telling our app to use bodyparser
 app.use(bodyParser.urlencoded({ 
