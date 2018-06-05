71854a7dc806c72e0cfe3f04194cd49050b795a6
KrisB <david.bierma@gmail.com>
2018-06-05 14:40:13 -0500

    update comments

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
     app.get('/api/friends', function (req,res) {
         console.log(friendsData);
 
-        res.json(friendsData);  //  
+        res.json(friendsData);  // getting a response back with string information from friends.js  
     });
 
     app.get('/api/you', function (req, res) {
@@ -15,7 +15,7 @@ module.exports = function (app) {
     })
 
     app.post("/api/friends", function(req, res) {  // when we create a post to this route, it runs this function
-        var newFriend = req.body;   // body came from the body parser function
+        var newFriend = req.body;   // 'body' came from body parser
         var parsedFriend = newFriend.scores.map(function (val, index) {
             return parseInt(val);
         });
